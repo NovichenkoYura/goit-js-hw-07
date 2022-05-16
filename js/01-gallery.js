e0d9a8816c03f12b2410b64bf8935b0f1a7f78df
NovichenkoYura <novichenko.y@gmail.com>
2022-05-16 17:51:02 +0300

    started

---
+++ js/01-gallery.js
@@ -0,0 +1,29 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+
+// console.log(createGalleryItemsMarkup(galleryItems))
+
+const galleryList = document.querySelector('.gallery');
+const ItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', ItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href=${original}">
+            <img
+            class="gallery__image"
+            src="small-image.jpg"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join('');
+   
+}
