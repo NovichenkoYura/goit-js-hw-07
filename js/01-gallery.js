680b77dff1d6922ccf52d0b0298c8ffeececafa6
NovichenkoYura <novichenko.y@gmail.com>
2022-05-17 19:14:09 +0300

    невдала спроба підключення лайтбокса

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,14 +7,17 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 const ItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
 galleryList.insertAdjacentHTML('beforeend', ItemsMarkup);
+
+galleryList.addEventListener('click', onImageItemClick)
 
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems
         .map(({ original, description }) => {
         return `
     <div class="gallery__item">
-        <a class="gallery__link" href=${original}">
+        <a class="gallery__link" href="${original}">
             <img
             class="gallery__image"
             src="small-image.jpg"
@@ -25,5 +28,12 @@ function createGalleryItemsMarkup(galleryItems) {
     </div>
     `;
     }).join('');
-   
 }
+
+// function onImageItemClick(evt) {
+//     if (!evt.target.classList.contains('gallery__image')) {
+//         return;
+//     }
+//     console.log(evt.target);
+// }
+
